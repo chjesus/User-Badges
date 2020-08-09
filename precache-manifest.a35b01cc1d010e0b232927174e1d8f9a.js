cf9597cbab9342c30f18e01635b425b8778af4d5
Jesus Chacon <chjesussao@gmail.com>
2020-08-09 12:21:04 -0400

    Updates

---
+++ precache-manifest.a35b01cc1d010e0b232927174e1d8f9a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9581e0b9ae5af83c486e95de1f40f32d",
+    "url": "/user-badges/index.html"
+  },
+  {
+    "revision": "7c9422b24dc5eb5f24f1",
+    "url": "/user-badges/static/css/main.fddef87e.chunk.css"
+  },
+  {
+    "revision": "d0e840976cbe2e42a272",
+    "url": "/user-badges/static/js/2.7bde2c7f.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/user-badges/static/js/2.7bde2c7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c9422b24dc5eb5f24f1",
+    "url": "/user-badges/static/js/main.c2f535e4.chunk.js"
+  },
+  {
+    "revision": "5d0bcf3b21080b705978",
+    "url": "/user-badges/static/js/runtime-main.d8bb682c.js"
+  },
+  {
+    "revision": "1092a4fea3a511228f07862b78ea98a1",
+    "url": "/user-badges/static/media/logo1.1092a4fe.jpg"
+  },
+  {
+    "revision": "d15efd9b9f89d09485863314171edf5e",
+    "url": "/user-badges/static/media/logo4.d15efd9b.jpg"
+  }
+]);
